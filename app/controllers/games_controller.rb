7dd2cd3c2cf9bf789b8c3909b121a29efd520487
Chris Stull <chris.stull@gmail.com>
2018-10-01 17:33:16 -0400

    Some Games Controller logic

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -26,6 +26,23 @@ class GamesController < ApplicationController
   def update
     @game = Game.find_by(id: params[:id])
     byebug
+    #Logic for action resolution here
+    if action_params[:effect_type] == "Attack"
+      @game.boss_health -= action_params[:power].to_i
+    elsif action_params[:effect_type] == "Heal"
+      @game.user_health += action_params[:power].to_i
+    end
+    @game.save
+
+    if action_params[:quantity] == 1
+      Hand.find_by(id: action_params[:hand_id]).destroy
+      User.find_by(id: @game.user_id).hands << Hand.create(user_id: @game.user_id, card_library_id: rand(1..CardLibrary.all.size), quantity: 1)
+    else
+      Hand.find_by(id: action_params[:hand_id]).update(quantity: (action_params[:quantity].to_i-1))
+      User.find_by(id: @game.user_id).hands << Hand.create(user_id: @game.user_id, card_library_id: rand(1..CardLibrary.all.size), quantity: 1)
+    end
+    #Attack/Heal -> alter related @game values
+    #Update @game object
     redirect_to @game
   end
 
@@ -41,6 +58,10 @@ class GamesController < ApplicationController
 
   def game_params
     params.require(:game).require(:user_health, :boss_health, :user_id, :boss_id, :image, :bosses_defeated)
+  end
+
+  def action_params
+    params.require(:hand).permit(:effect_type, :power, :hand_id, :quantity)
   end
 
 end
