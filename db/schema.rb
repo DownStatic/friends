13c01ad59ee622bd49bd10778a394e48d89c3d0f
Chris Stull <chris.stull@gmail.com>
2019-03-13 13:45:30 -0400

    Change db from sqlite3 to pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_10_03_202708) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "boss_phrases", force: :cascade do |t|
     t.integer "boss_id"
     t.string "phrase"
